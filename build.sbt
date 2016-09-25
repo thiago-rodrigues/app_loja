06d20c71e7465c3553c86f40b2d1f59325e78612
thiago.ribeiro <trr.rodrigues@gmail.com>
2016-09-25 09:14:05 -0300

    configura orm eben com postgres

--- build.sbt
+++ build.sbt
@@ -2,14 +2,15 @@ name := """play-java"""
 
 version := "1.0-SNAPSHOT"
 
-lazy val root = (project in file(".")).enablePlugins(PlayJava)
+lazy val root = (project in file(".")).enablePlugins(PlayJava, PlayEbean)
 
 scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq(
   javaJdbc,
   cache,
-  javaWs
+  javaWs,
+  "org.postgresql" % "postgresql" % "9.4-1206-jdbc42"
 )
 
 
---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,17 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table produto (
+  id                            bigserial not null,
+  nome                          varchar(255),
+  valor                         decimal(38),
+  constraint pk_produto primary key (id)
+);
+
+
+# --- !Downs
+
+drop table if exists produto cascade;
+
