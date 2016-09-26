9203462b2de750c103f43c20c9e5fe0283e3d544
Thiago Ribeiro <thiago.ribeiro@sisqualis.com.br>
2016-09-26 09:28:16 -0300

    Testa envio do objeto produto para viewer

--- app/controllers/ProdutoController.java
+++ app/controllers/ProdutoController.java
@@ -1,5 +1,6 @@
 package controllers;
 import models.Produto;
+import play.Logger;
 import play.mvc.*;
 import views.html.produtos.indexProdutos;
 import java.util.List;
@@ -8,6 +9,7 @@ public class ProdutoController extends Controller {
 
     public Result index() {
         List<Produto> produtos_list = Produto.find.all();
+        Logger.info("Produtos"+ produtos_list);
         return ok(indexProdutos.render(produtos_list));
     }
 
