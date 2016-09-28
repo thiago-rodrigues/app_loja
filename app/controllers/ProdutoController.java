726ef3e86d91e74d696bbc1cd3769b991454db1a
Thiago Ribeiro <thiago.ribeiro@sisqualis.com.br>
2016-09-28 16:25:08 -0300

    adiciona funcao atualizar

--- app/controllers/ProdutoController.java
+++ app/controllers/ProdutoController.java
@@ -1,14 +1,16 @@
 package controllers;
 import models.Produto;
-import play.Logger;
 import play.data.DynamicForm;
-import play.data.Form;
 import play.data.FormFactory;
 import play.mvc.*;
-import views.html.produtos.indexProdutos;
+import views.html.produtos.deletar;
+import views.html.produtos.editar;
+import views.html.produtos.listar;
+import views.html.produtos.novo;
 
 import javax.inject.Inject;
 import java.math.BigDecimal;
+import java.security.PublicKey;
 import java.util.List;
 
 public class ProdutoController extends Controller {
@@ -16,20 +18,36 @@ public class ProdutoController extends Controller {
     @Inject
     FormFactory formFactory;
 
-    public Result index() {
-        List<Produto> produtos_list = Produto.find.all();
-        Logger.info("Produtos"+ produtos_list);
-        return ok(indexProdutos.render(produtos_list));
+    public Result listar() {
+        List<Produto> produtos_list = Produto.find.orderBy("id desc").findList();
+        return ok(listar.render(produtos_list));
     }
 
-    public Result addProduto(){
+    public Result adicionar(){
         DynamicForm form = formFactory.form().bindFromRequest();
         Produto produto = new Produto();
         produto.nome = form.get("nome");
         produto.valor = new BigDecimal(form.get("valor"));
         produto.save();
+        return redirect(routes.ProdutoController.listar());
+    }
+    public Result novo(){
+        return ok(novo.render("Cadastro Produto"));
+    }
 
-        return redirect(routes.ProdutoController.index());
+    public Result editar(Long id){
+        Produto produto = Produto.find.byId(id);
+        return ok(editar.render(produto));
+    }
+
+    public Result atualizar(Long id){
+        DynamicForm form = formFactory.form().bindFromRequest();
+        Produto produto = new Produto();
+        produto.id = new Long(form.get("id"));
+        produto.nome = form.get("nome");
+        produto.valor = new BigDecimal(form.get("valor"));
+        produto.update();
+        return redirect(routes.ProdutoController.listar());
     }
 }
 
