08393aa75c4ecc1f71274b18133c9732996e23e5
Thiago Ribeiro <thiago.ribeiro@sisqualis.com.br>
2016-09-29 17:13:46 -0300

    adicina em todas as ações a verificação da sessão

--- app/controllers/ProdutoController.java
+++ app/controllers/ProdutoController.java
@@ -1,5 +1,6 @@
 package controllers;
 import models.Produto;
+import models.Usuario;
 import play.data.DynamicForm;
 import play.data.FormFactory;
 import play.mvc.*;
@@ -18,104 +19,131 @@ public class ProdutoController extends Controller {
     @Inject
     FormFactory formFactory;
 
+
     public Result listar() {
-        beginTransaction();
-        try {
-            List<Produto> produtos_list = Produto.find.orderBy("id desc").findList();
-            commitTransaction();
-            return ok(listar.render(produtos_list));
-        }catch(Exception e) {
-            rollbackTransaction();
-            return null;
-        }finally {
-            endTransaction();
+        Usuario usuario = new Usuario();
+        if(usuario.vericaSessaoUsuario() != null) {
+            beginTransaction();
+            try {
+                List<Produto> produtos_list = Produto.find.orderBy("id desc").findList();
+                commitTransaction();
+                return ok(listar.render(produtos_list));
+            } catch (Exception e) {
+                rollbackTransaction();
+                return null;
+            } finally {
+                endTransaction();
+            }
         }
+        return redirect(routes.LoginController.login());
     }
 
     public Result adicionar(){
-        beginTransaction();
-        try{
-            DynamicForm form = formFactory.form().bindFromRequest();
-            Produto produto = new Produto();
-            produto.nome = form.get("nome");
-            produto.valor = new BigDecimal(form.get("valor"));
-            produto.save();
-            commitTransaction();
-            return redirect(routes.ProdutoController.listar());
-        }catch(Exception e){
-            rollbackTransaction();
-            return null;
+        Usuario usuario = new Usuario();
+        if(usuario.vericaSessaoUsuario() != null) {
+            beginTransaction();
+            try {
+                DynamicForm form = formFactory.form().bindFromRequest();
+                Produto produto = new Produto();
+                produto.nome = form.get("nome");
+                produto.valor = new BigDecimal(form.get("valor"));
+                produto.save();
+                commitTransaction();
+                return redirect(routes.ProdutoController.listar());
+            } catch (Exception e) {
+                rollbackTransaction();
+                return null;
+            } finally {
+                endTransaction();
+            }
         }
-        finally {
-            endTransaction();
-        }
+        return redirect(routes.LoginController.login());
     }
 
     public Result novo(){
-        return ok(novo.render("Cadastro Produto"));
+        Usuario usuario = new Usuario();
+        if(usuario.vericaSessaoUsuario() != null) {
+            return ok(novo.render("Cadastro Produto"));
+        }
+        return redirect(routes.LoginController.login());
     }
 
     public Result editar(Long id){
-        beginTransaction();
-        try {
-            Produto produto = Produto.find.byId(id);
-            commitTransaction();
-            return ok(editar.render(produto));
-        }catch(Exception e) {
-            rollbackTransaction();
-            return null;
-        }finally {
-            endTransaction();
+        Usuario usuario = new Usuario();
+        if(usuario.vericaSessaoUsuario() != null) {
+            beginTransaction();
+            try {
+                Produto produto = Produto.find.byId(id);
+                commitTransaction();
+                return ok(editar.render(produto));
+            } catch (Exception e) {
+                rollbackTransaction();
+                return null;
+            } finally {
+                endTransaction();
+            }
         }
+        return redirect(routes.LoginController.login());
     }
 
     public Result atualizar(Long id){
-        beginTransaction();
-        try {
-            DynamicForm form = formFactory.form().bindFromRequest();
-            Produto produto = new Produto();
-            produto.id = new Long(form.get("id"));
-            produto.nome = form.get("nome");
-            produto.valor = new BigDecimal(form.get("valor"));
-            produto.update();
-            commitTransaction();
-            return redirect(routes.ProdutoController.listar());
-        }catch(Exception e) {
-            rollbackTransaction();
-            return null;
-        }finally {
-            endTransaction();
+        Usuario usuario = new Usuario();
+        if(usuario.vericaSessaoUsuario() != null) {
+            beginTransaction();
+            try {
+                DynamicForm form = formFactory.form().bindFromRequest();
+                Produto produto = new Produto();
+                produto.id = new Long(form.get("id"));
+                produto.nome = form.get("nome");
+                produto.valor = new BigDecimal(form.get("valor"));
+                produto.update();
+                commitTransaction();
+                return redirect(routes.ProdutoController.listar());
+            } catch (Exception e) {
+                rollbackTransaction();
+                return null;
+            } finally {
+                endTransaction();
+            }
         }
+        return redirect(routes.LoginController.login());
     }
 
     public Result deletar(Long id){
-        beginTransaction();
-        try {
-            Produto.find.ref(id).delete();
-            commitTransaction();
-            return redirect(routes.ProdutoController.listar());
-        }catch(Exception e) {
-            rollbackTransaction();
-            return null;
-        }finally {
-            endTransaction();
+        Usuario usuario = new Usuario();
+        if(usuario.vericaSessaoUsuario() != null) {
+            beginTransaction();
+            try {
+                Produto.find.ref(id).delete();
+                commitTransaction();
+                return redirect(routes.ProdutoController.listar());
+            } catch (Exception e) {
+                rollbackTransaction();
+                return null;
+            } finally {
+                endTransaction();
+            }
         }
+        return redirect(routes.LoginController.login());
     }
 
     public Result formDeletar(Long id){
-        beginTransaction();
-        try {
-            Produto produto = Produto.find.byId(id);
-            commitTransaction();
-            return ok(deletar.render(produto));
-        }catch(Exception e) {
-            rollbackTransaction();
-            return null;
-        }finally {
-            endTransaction();
+        Usuario usuario = new Usuario();
+        if(usuario.vericaSessaoUsuario() != null) {
+            beginTransaction();
+            try {
+                Produto produto = Produto.find.byId(id);
+                commitTransaction();
+                return ok(deletar.render(produto));
+            } catch (Exception e) {
+                rollbackTransaction();
+                return null;
+            } finally {
+                endTransaction();
+            }
         }
+        return redirect(routes.LoginController.login());
     }
-
 
 }
 
