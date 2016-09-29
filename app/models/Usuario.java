a31c03a158412af395e90049b827edaf7d813a6f
Thiago Ribeiro <thiago.ribeiro@sisqualis.com.br>
2016-09-29 16:02:01 -0300

    implementa função que valida usuario

--- app/models/Usuario.java
+++ app/models/Usuario.java
@@ -1,19 +1,24 @@
 package models;
 import com.avaje.ebean.Finder;
 import com.avaje.ebean.Model;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Usuario extends Model{
-
-
+    @Id
+    public Long id;
+    @Column(unique=true)
     public String usuario;
     public String senha;
     public static Finder<Long, Usuario> find = new Finder(Usuario.class);
 
-    public Usuario validaUsuario(String usuario, String senha){
-        Usuario usuarioLogin = new Usuario();
-        return  usuarioLogin;
+    public static Usuario validaUsuario(String usuario, String senha) {
+        Usuario usuarioLogin = Usuario.find.where().eq("usuario",usuario).eq("senha",senha).findUnique();
+        return usuarioLogin;
     }
-
 }
+
+
