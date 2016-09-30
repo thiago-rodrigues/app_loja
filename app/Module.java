f22a0e6aedcc65a4568607f0caca8abc2add668f
Thiago Ribeiro <thiago.ribeiro@sisqualis.com.br>
2016-09-30 17:07:36 -0300

    adiciona função que chama a alimentação do banco

--- app/Module.java
+++ app/Module.java
@@ -1,10 +1,11 @@
 import com.google.inject.AbstractModule;
+import utils.inicializacao.AlimentarBanco;
 
 public class Module extends AbstractModule {
 
     @Override
     public void configure() {
-
+        bind(AlimentarBanco.class).asEagerSingleton();
     }
 
 }
